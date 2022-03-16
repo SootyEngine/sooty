e70da8f0fc781a8e8c68eb6ff59f80051802fdcd
teebarjunk <teebarjunk@github.com>
2022-03-15 21:53:33 -0400

    debug only show modified

--- addons/sooty_engine/debug/ui_debug.gd
+++ addons/sooty_engine/debug/ui_debug.gd
@@ -2,11 +2,17 @@ extends Node
 
 var lines := []
 var filter := ""
+var only_modified := true
 
 func _ready() -> void:
 	$VBoxContainer/HBoxContainer/Button.pressed.connect(_update)
+	$VBoxContainer/HBoxContainer/CheckButton.toggled.connect(_toggled)
 	$VBoxContainer/HBoxContainer/LineEdit.text_changed.connect(_filter_changed)
 	State.changed_from_to.connect(_changed)
+
+func _toggled(t):
+	only_modified = t
+	_update()
 
 func _changed(property, from, to):
 	print("Changed %s to %s from %s." % [property, to, from])
@@ -17,7 +23,7 @@ func _filter_changed(t: String):
 	_redraw()
 
 func _update():
-	var state := State._get_state()
+	var state := State._get_changed_states() if only_modified else State._get_state()
 	lines = JSON.new().stringify(state, "\t", false).split("\n")
 	_redraw()
 	
